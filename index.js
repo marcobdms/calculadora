c7dea09e6f081c2fae46a20abd456d05d808b58f
marcobdms <el_marco4@hotmail.com>
2023-05-29 19:43:35 +0200

    null

---
+++ index.js
@@ -0,0 +1,63 @@
+
+let temporal = 0;
+let operacion = '';
+var input = document.getElementById('input');
+
+function add(num) {
+  if (num == 0) {
+    if (input.value[input.value.length - 1] != '0' || input.value.length > 1) {
+      input.value = input.value + num;
+    }
+  } else if (num == '.') {
+    if (input.value[input.value.length - 1] != '.' && input.value != '') {
+      input.value = input.value + num;
+    }
+  } else {
+    input.value = input.value + num;
+  }
+}
+
+function operacionFunction(operador) {
+  operacion = operador;
+  temporal = input.value;
+  input.value = '';
+}
+
+function igual() {
+  if (operacion != '') {
+    input.value = eval(temporal + operacion + input.value);
+  }
+}
+
+function inverso() {
+  input.value = input.value * -1;
+}
+
+function porciento() {
+  input.value = input.value / 100;
+}
+
+function cuadrado() {
+  input.value = input.value ** 2;
+}
+
+function cubo() {
+  input.value = input.value ** 3;
+}
+
+function raiz() {
+  input.value = input.value ** 0.5;
+}
+
+function PI() {
+  input.value = 3.14596;
+}
+
+function Ce() {
+  if (input.value != '') {
+    input.value = '';
+  } else {
+    temporal = 0;
+    operacion = '';
+  }
+}
